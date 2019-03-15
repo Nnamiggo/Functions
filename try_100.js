6f9b162707ce903c18476f8fb13fd8570e1bbefe
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-15 15:46:31 +0300

    copying my file

---
+++ try_100.js
@@ -0,0 +1,15 @@
+/*
+Write a loop which prompts for a number greater than 100. If the visitor enters another number – 
+ask them to input again.
+ The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the 
+ input/enters an empty line.
+
+>> At first, we can assume that the visitor only inputs numbers and there’s no need to implement 
+a special handling for a non-numeric input in this task.
+
+++ As a bonus, Can you add that feature to your code.
+*/
+
+
+//Create variabble to hold user input var user-input
+//if var user-input is greator than 100 stop else ask for input again
