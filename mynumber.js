eacb3ef0b99d8d7fc5f830176f7b5fc7666ba094
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-18 11:19:12 +0300

    editing the file

--- mynumber.js
+++ mynumber.js
@@ -8,28 +8,37 @@ In this task we assume that the input is always a number.
 Save the file as null_checker in the same folder that you saved yesterday's work 
 */
 
-// console.log("my favorite number is 7") // This for the user to see what is happening
 
-// var mynumber = 7;  // initialise the number to seven why? it is my favorite number
+// function mynumber(x) {
+	
+// 	console.log("My favorite number is " +x); // This for the user to see what is happening
+	
+// 	var test;
 
-// if (mynumber>0) 
+// if (x>0) 
 
 // 	{
 	
-// 	console.log(1 +"-----this means My favorite number is greator than zero"); //Execute this block if my number is grater than 0
+// 	test = (1 +"-----this means My favorite number is greator than zero"); //Execute this block if my number is grater than 0
+
 // 	}
 
-// 	else if (mynumber<0)
+// 	else if (x<0)
 // 	{
-// 	console.log (-1 +"-----this means My favorite number is less than zero") //Execute this block if my number is less than 0
+// 	test = (-1 +"-----this means My favorite number is less than zero") //Execute this block if my number is less than 0
+	
 // 	}
 
 // 	else
 // 	{
-// 		console.log(0 +"-----this means My favorite number is equal to zero");  //and this block if my number is equal to zero
+// 		test=( 0 +"-----this means My favorite number is equal to zero");  //and this block if my number is equal to zero
+		 
 // 	}
-
-function mynumber(x) {
+	
+// 	console.log(test);
+// }
+// mynumber(7)
+mynumber=(x)=> {
 	
 	console.log("My favorite number is " +x); // This for the user to see what is happening
 	
@@ -40,7 +49,7 @@ if (x>0)
 	{
 	
 	test = (1 +"-----this means My favorite number is greator than zero"); //Execute this block if my number is grater than 0
-	
+
 	}
 
 	else if (x<0)
